65e4d7fe15d5b0713d0f92aae575ff311cbca36c
Damir15 <krosava_15d@mail.ru>
2019-05-21 18:01:02 +0600

    Add new files

---
+++ manager/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+echo '<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>App</title>
+</head>
+<body>
+    <h1>Hello, world!</h1>
+</body>
+</html>';
